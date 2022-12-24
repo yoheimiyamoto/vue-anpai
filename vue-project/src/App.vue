9925e6b9425ac66d327bd952cba724d21f92f101
yoheimiyamoto <yoheimiyamoto@gmail.com>
2022-12-25 00:01:11 +0900

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -10,10 +10,6 @@ export default {
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-    </div>
   </header>
 
   <main>
