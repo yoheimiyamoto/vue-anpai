0b8747ccc87660c6226452c924af650512a6e2b3
yoheimiyamoto <yoheimiyamoto@gmail.com>
2022-12-25 09:21:00 +0900

    update

--- vue-project/src/components/Tiles.vue
+++ vue-project/src/components/Tiles.vue
@@ -10,15 +10,15 @@ export default {
   } {
     return {
       tiles: [
-        { id: id++, text: 1, selected: false },
-        { id: id++, text: 2, selected: false },
-        { id: id++, text: 3, selected: false },
-        { id: id++, text: 4, selected: false },
-        { id: id++, text: 5, selected: false },
-        { id: id++, text: 6, selected: false },
-        { id: id++, text: 7, selected: false },
-        { id: id++, text: 8, selected: false },
-        { id: id++, text: 9, selected: false }
+        { id: id++, text: 1, selected: false, anpai: false,  kiken: false},
+        { id: id++, text: 2, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 3, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 4, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 5, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 6, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 7, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 8, selected: false, anpai: false,  kiken: false },
+        { id: id++, text: 9, selected: false, anpai: false,  kiken: false }
       ],
 
       safe_numbers: [],
@@ -37,12 +37,36 @@ export default {
       // 安牌の取得
       this.safe_numbers = get_anpai(selected_number)
       this.kiken_numbers = get_kiken_hai(selected_number)
+
+      // 危険牌のフラグ追加
+      for (let i = 0; i < this.kiken_numbers.length; i++) {
+        const kiken_number = this.kiken_numbers[i]
+        let found = this.tiles.find((tile) => {
+          return tile.text == kiken_number
+        })
+        if(found) {
+          found.kiken = true
+        }
+      }
+
+      // 安牌のフラグ追加
+      for (let i = 0; i < this.safe_numbers.length; i++) {
+        const safe_number = this.safe_numbers[i]
+        let found = this.tiles.find((tile) => {
+          return tile.text == safe_number
+        })
+        if(found) {
+          found.anpai = true
+        }
+      }
     },
 
     // 選択した牌をすべて未選択に変更
     clearSelectedTiles() {
       this.tiles.forEach((tile: any, index: number) => {
         tile.selected = false
+        tile.anpai = false
+        tile.kiken = false
       })
       this.safe_numbers = []
     }
@@ -55,7 +79,7 @@ export default {
     <div class="card-body">
       <h5 class="card-title"><slot></slot></h5>
       <div class="tiles" v-for="tile in tiles" :key="tile.id">
-        <button v-bind:class="{selected: tile.selected}" @click="clickTile(tile)">{{tile.text}}</button>
+        <button v-bind:class="{selected: tile.selected, kiken: tile.kiken, anpai: tile.anpai}" @click="clickTile(tile)">{{tile.text}}</button>
       </div>
       <div class="suji">
         <p>安牌</p>
@@ -68,9 +92,9 @@ export default {
         <p class="h5" v-else>なし</p>
       </div>
       <button class="btn btn-primary" @click="clearSelectedTiles">Clear</button>
-      <!-- <div>
+      <div>
         {{tiles}}
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -94,6 +118,14 @@ span {
 }
 
 .selected {
+  color: green;
+}
+
+.anpai {
+  color: blue;
+}
+
+.kiken {
   color: red;
 }
 
